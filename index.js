e40a065a5cc7918a60461c1c31c082b54f0756c5
MaenSZubaydi <maenzubaydi@gmail.com>
2022-04-17 12:17:54 +1000

    new

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(blockNumber) {
+    const hQNumber = 42;
+    return Math.abs(hQNumber-blockNumber);
+}
+
+function distanceFromHqInFeet(blockNumber) {
+  const feetConversionRate = 264;
+  return distanceFromHqInBlocks(blockNumber) * feetConversionRate;
+}
+
+function distanceTravelledInFeet(startBlock, endBlock) {
+  const feetConversionRate = 264;
+  return Math.abs(startBlock-endBlock)*feetConversionRate;
+}
+
+function calculatesFarePrice(start, destination) {
+  let distance = distanceTravelledInFeet(start, destination);
+  const freeTravelDistance = 400;
+  const freeFare = 0;
+  const farePerFoot = 0.02;
+  const flatFare = 25;
+  const travelOverLimitMessage = 'cannot travel that far';
+
+  if(distance<400){
+      return freeFare;
+  } else if (distance>=400 && distance<=2000){
+    return (distance-freeTravelDistance)*farePerFoot;
+  } else if (distance>2000 && distance<=2500){
+    return flatFare;
+  } else {
+    return travelOverLimitMessage;
+  }
+
+}
+
+
+
